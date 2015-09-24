4cbf9a8c18769fd82f4a7fa943b9699a96c16b3f
Hina Sakazaki <hsakazaki@zynga.com>
2015-09-23 20:08:50 -0700

    got some done

---
+++ GithubDemo/GithubRepoViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GithubRepoViewCell.swift
+//  GithubDemo
+//
+//  Created by Hina Sakazaki on 9/23/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class GithubRepoViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
